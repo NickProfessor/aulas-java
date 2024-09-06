fccac74983ccc4dddba9a7f73e6e3257d51f8b1a
Nickollas <nickollassilva90@gmail.com>
2024-09-05 21:08:10 -0300

    Primeiro programa em java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static String nome = "Nickollas";
+
+    public static void main(String[] args) {
+        System.out.println("Olá, "+ nome);
+        System.out.println("Tudo bem, "+ nome +"?");
+    }
+
+
+}
---
+++ src/Soma.java
@@ -0,0 +1,5 @@
+public class Soma {
+    public static void main(String[] args){
+        System.out.println("O valor da soma 10+5 é igual a "+ (10 + 5));
+    }
+}
